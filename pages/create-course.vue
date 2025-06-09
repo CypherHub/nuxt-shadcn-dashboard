e6018376ad3c43a12a52a87b920ad3b7cf2e4065
CypherHub <admin@cypherhub.io>
2025-06-09 10:57:37 -0400

    create course added

---
+++ pages/create-course.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="w-full flex flex-col items-stretch gap-4">
+    Create Course Page
+  </div>
+</template>
+
+<style scoped>
+
+</style>
