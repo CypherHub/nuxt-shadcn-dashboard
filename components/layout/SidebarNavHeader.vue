6930f79ec2daecc65e8da0e454cef9937c2e33d9
Thisara_Ruwanpathirana <thisararpg@gmail.com>
2025-06-10 22:03:07 +0530

    when click on AcmeInc it should only have one option and not cickable

--- components/layout/SidebarNavHeader.vue
+++ components/layout/SidebarNavHeader.vue
@@ -19,11 +19,10 @@ const activeTeam = ref(props.teams[0])
     <SidebarMenuItem>
       <DropdownMenu>
         <DropdownMenuTrigger as-child>
-          <SidebarMenuButton
-            size="lg"
-            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-          >
-            <div class="aspect-square size-8 flex items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+          <SidebarMenuButton size="lg"
+            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
+            <div
+              class="aspect-square size-8 flex items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
               <Icon :name="activeTeam.logo" class="size-4" />
             </div>
             <div class="grid flex-1 text-left text-sm leading-tight">
@@ -35,26 +34,36 @@ const activeTeam = ref(props.teams[0])
             <Icon name="i-lucide-chevrons-up-down" class="ml-auto" />
           </SidebarMenuButton>
         </DropdownMenuTrigger>
-        <DropdownMenuContent
-          class="min-w-56 w-[--radix-dropdown-menu-trigger-width] rounded-lg"
-          align="start"
-          :side="isMobile ? 'bottom' : 'right'"
-        >
+        <DropdownMenuContent class="min-w-56 w-[--radix-dropdown-menu-trigger-width] rounded-lg" align="start"
+          :side="isMobile ? 'bottom' : 'right'">
           <DropdownMenuLabel class="text-xs text-muted-foreground">
             Teams
           </DropdownMenuLabel>
-          <DropdownMenuItem
+          <!-- <DropdownMenuItem
             v-for="(team, index) in teams"
             :key="team.name"
             class="gap-2 p-2"
             @click="activeTeam = team"
           >
+          
             <div class="size-6 flex items-center justify-center border rounded-sm">
               <Icon :name="team.logo" class="size-4 shrink-0" />
             </div>
             {{ team.name }}
             <DropdownMenuShortcut>⌘{{ index + 1 }}</DropdownMenuShortcut>
+          </DropdownMenuItem> -->
+          <DropdownMenuItem v-for="(team, index) in teams" :key="team.name" class="gap-2 p-2" :class="{
+            'pointer-events-none cursor-not-allowed': team.name === activeTeam.name
+          }" @click="team.name !== activeTeam.name && (activeTeam = team)">
+            <div class="size-6 flex items-center justify-center border rounded-sm">
+              <Icon :name="team.logo" class="size-4 shrink-0" />
+            </div>
+            {{ team.name }}
+            <DropdownMenuShortcut v-if="team.name !== activeTeam.name">
+              ⌘{{ index + 1 }}
+            </DropdownMenuShortcut>
           </DropdownMenuItem>
+
           <DropdownMenuSeparator />
           <!-- <DropdownMenuItem class="gap-2 p-2">
             <div class="size-6 flex items-center justify-center border rounded-md bg-background">
@@ -70,6 +79,4 @@ const activeTeam = ref(props.teams[0])
   </SidebarMenu>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
